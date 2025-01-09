9341dd3d8e8c1538237ea333f0b43d1289c2eca8
A1909bek <aa2004bek@gmail.com>
2025-01-09 13:44:06 +0000

    committing

---
+++ largest-positive-integer(ex2441).py
@@ -0,0 +1,7 @@
+class Solution:
+    def findMaxK(self, nums: List[int]) -> int:
+        nums.sort(reverse=True)
+        for num in nums:
+            if -num in nums:
+                return num
+        return -1
