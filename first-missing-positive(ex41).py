c4004373618b5a2a058acdccc4040e895e9b8b1c
A1909bek <aa2004bek@gmail.com>
2025-01-12 19:45:54 +0000

    commit

---
+++ first-missing-positive(ex41).py
@@ -0,0 +1,6 @@
+class Solution:
+    def firstMissingPositive(self, nums: List[int]) -> int:
+        nums_set = set(nums)
+        for i in range(1, len(nums) + 2): 
+            if i not in nums_set:
+                return i
