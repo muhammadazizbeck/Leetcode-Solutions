029b67372adcda792501a0f97156f0cc5354bf1d
A1909bek <aa2004bek@gmail.com>
2025-02-28 12:02:12 +0500

    next commit

---
+++ find-the-index-of-the-first-occurence-in-a-string(ex28).py
@@ -0,0 +1,8 @@
+class Solution:
+    def strStr(self, haystack: str, needle: str) -> int:
+        length = len(needle)
+        for i in range(len(haystack) - length + 1):
+            if haystack[i:i+length] == needle:
+                return i 
+        
+        return -1
