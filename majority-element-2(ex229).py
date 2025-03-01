b3a756b5d44472fb3c8fa32396b633d1b4e87236
A1909bek <aa2004bek@gmail.com>
2025-03-01 10:21:28 +0500

    committing

--- majority-element-2(ex229).py
+++ majority-element-2(ex229).py
@@ -1,4 +1,11 @@
+# class Solution:
+#     def majorityElement(self, nums: List[int]) -> List[int]:
+#         final = [num for num in nums if nums.count(num)>len(nums)/3]
+#         return list(set(final))
+
 class Solution:
     def majorityElement(self, nums: List[int]) -> List[int]:
-        final = [num for num in nums if nums.count(num)>len(nums)/3]
-        return list(set(final))
+        counter = Counter(nums)
+        n = len(nums)
+        txt = [key for key,val in counter.items() if val>n//3]
+        return txt
