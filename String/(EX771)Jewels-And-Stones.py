28538181c5fa39e44445bd98b6ed0c501c775ba1
A1909bek <aa2004bek@gmail.com>
2025-10-28 15:30:21 +0500

    adding solution

---
+++ String/(EX771)Jewels-And-Stones.py
@@ -0,0 +1,16 @@
+class Solution(object):
+    def numJewelsInStones(self, jewels, stones):
+        """
+        :type jewels: str
+        :type stones: str
+        :rtype: int
+        """
+        total = 0
+        seen = set()
+        final = Counter(stones)
+
+        for stone, count in final.items():
+            if stone in jewels and stone not in seen:
+                total += count
+            seen.add(stone)
+        return total
