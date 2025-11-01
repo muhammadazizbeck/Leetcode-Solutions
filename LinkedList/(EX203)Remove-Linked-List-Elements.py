f4e14ee810090cb02f860226fd990e42b764b45e
A1909bek <aa2004bek@gmail.com>
2025-11-01 22:24:20 +0500

    adding solution

---
+++ LinkedList/(EX203)Remove-Linked-List-Elements.py
@@ -0,0 +1,22 @@
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution(object):
+    def removeElements(self, head, val):
+        """
+        :type head: Optional[ListNode]
+        :type val: int
+        :rtype: Optional[ListNode]
+        """
+        helper = ListNode(0)
+        helper.next = head
+        curr = helper
+
+        while curr.next:
+            if curr.next.val == val:
+                curr.next = curr.next.next
+            else:
+                curr = curr.next
+        return helper.next
