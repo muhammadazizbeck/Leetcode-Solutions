c122c09ce13a055cb7a97da1d937762d7101268f
A1909bek <aa2004bek@gmail.com>
2025-11-01 18:58:03 +0500

    adding a solution

---
+++ LinkedList/(EX1290)Convert-Binary-Number-In-A-Linked-List-To-Integer.py
@@ -0,0 +1,17 @@
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution(object):
+    def getDecimalValue(self, head):
+        """
+        :type head: Optional[ListNode]
+        :rtype: int
+        """
+        curr = head
+        final = ""
+        while curr:
+            final += str(curr.val)
+            curr = curr.next
+        return int(final,2)
