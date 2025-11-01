1ecc70b35dd66f0c812c8bdf0ef69d22b264e0ea
A1909bek <aa2004bek@gmail.com>
2025-11-01 20:07:52 +0500

    adding solution

---
+++ LinkedList/(EX876)Middle-Of-The-Linked-List.py
@@ -0,0 +1,16 @@
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution(object):
+    def middleNode(self, head):
+        """
+        :type head: Optional[ListNode]
+        :rtype: Optional[ListNode]
+        """
+        regular = double = head
+        while double and double.next:
+            regular = regular.next
+            double = double.next.next
+        return regular
