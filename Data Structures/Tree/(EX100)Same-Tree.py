15cc22ad6af65e5e6c6097505fa66d11f1d8a10c
A1909bek <aa2004bek@gmail.com>
2025-11-10 11:02:24 +0500

    adding a solution

---
+++ Data Structures/Tree/(EX100)Same-Tree.py
@@ -0,0 +1,34 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def isSameTree(self, p: Optional[TreeNode], q: Optional[TreeNode]) -> bool:
+        if not p and not q:
+            return True
+        
+        if not p or not q:
+            return False
+
+        if p.val != q.val:
+            return False
+
+        return self.isSameTree(p.left,q.left) and self.isSameTree(p.right,q.right)
+
+        # queue = deque([(p,q)])
+
+        # while queue:
+        #     node1,node2 = queue.popleft()
+
+        #     if not node1 and not node2:
+        #         continue
+
+        #     if not node1 or not node2 or node1.val != node2.val:
+        #         return False
+
+        #     queue.append((node1.left,node2.left))        
+        #     queue.append((node1.right,node2.right))
+
+        # return True
