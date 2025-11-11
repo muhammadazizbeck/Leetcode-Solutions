efd8ea7606614f4d48792c02a8442d5a66244b86
A1909bek <aa2004bek@gmail.com>
2025-11-11 11:50:18 +0500

    adding solution

---
+++ Data Structures/Tree/(EX404)Sum-Of-Left-Leaves.py
@@ -0,0 +1,25 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def sumOfLeftLeaves(self, root: Optional[TreeNode]) -> int:
+        if not root:
+            return 0
+
+        queue = deque([root])
+        total = 0
+        while queue:
+            node = queue.popleft()
+
+            if node.left and not node.left.left and not node.left.right:
+                total += node.left.val
+            
+            if node.left:
+                queue.append(node.left)
+            if node.right:
+                queue.append(node.right)
+
+        return total
