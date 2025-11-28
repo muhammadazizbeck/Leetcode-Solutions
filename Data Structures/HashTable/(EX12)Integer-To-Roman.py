a1aa1e16a212440325918cadff1141d371d04c40
A1909bek <aa2004bek@gmail.com>
2025-11-28 09:13:04 +0500

    adding solution

---
+++ Data Structures/HashTable/(EX12)Integer-To-Roman.py
@@ -0,0 +1,22 @@
+class Solution:
+    def intToRoman(self, num: int) -> str:
+        final = ""
+        values = [
+        1000, 900, 500, 400,
+        100,  90,  50,  40,
+        10,   9,   5,   4,
+        1
+        ]
+        symbols = [
+            "M", "CM", "D", "CD",
+            "C", "XC", "L", "XL",
+            "X", "IX", "V", "IV",
+            "I"
+        ]
+
+        for i in range(len(values)):
+            while num>=values[i]:
+                final += symbols[i]
+                num -= values[i]
+            
+        return final
