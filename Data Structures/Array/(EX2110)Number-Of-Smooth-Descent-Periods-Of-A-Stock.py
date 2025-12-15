2016e79d6e5dfe7cae379706f33866d0f54f35f1
A1909bek <aa2004bek@gmail.com>
2025-12-15 21:48:57 +0500

    adding solution

---
+++ Data Structures/Array/(EX2110)Number-Of-Smooth-Descent-Periods-Of-A-Stock.py
@@ -0,0 +1,14 @@
+class Solution:
+    def getDescentPeriods(self, prices: List[int]) -> int:
+        result = 0
+        length = 0
+
+        for i in range(len(prices)):
+            if i > 0 and prices[i] == prices[i - 1] - 1:
+                length += 1
+            else:
+                length = 1
+
+            result += length
+
+        return result
